88232082ccc61a1c9253d57e91436f5aff24e741
Eun Ji Shin <159417728+belovelace@users.noreply.github.com>
2024-11-19 16:56:08 +0900

    app

---
+++ app/app/src/test/java/com/commpass/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commpass.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
