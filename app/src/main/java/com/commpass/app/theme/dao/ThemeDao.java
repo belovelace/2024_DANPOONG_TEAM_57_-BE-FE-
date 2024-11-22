2d33a3646ab5535725824c07a28db925df4bb230
Eun Ji Shin <159417728+belovelace@users.noreply.github.com>
2024-11-23 00:33:03 +0900

    테마 조회 및 힐링테마 조회(더미 필요)

---
+++ app/src/main/java/com/commpass/app/theme/dao/ThemeDao.java
@@ -0,0 +1,22 @@
+package com.commpass.app.theme.dao;
+
+import com.commpass.app.theme.mapper.ThemeMapper;
+import com.commpass.app.theme.vo.ThemeVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ThemeDao {
+
+    private final ThemeMapper mapper;
+
+    public List<ThemeVo> getThemeList() {return mapper.getThemeList();}
+
+
+    public List<ThemeVo> getHealingList() {return mapper.getHealingList();}
+
+
+}//class
---
+++ app/src/main/java/com/commpass/app/theme/mapper/ThemeMapper.java
@@ -0,0 +1,20 @@
+package com.commpass.app.theme.mapper;
+
+import com.commpass.app.theme.vo.ThemeVo;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ThemeMapper {
+
+    @Select("SELECT theme_id, theme FROM travel_theme")
+    List<ThemeVo> getThemeList();
+
+    @Select("SELECT theme_id, theme_area, theme_name, lng, lat  FROM healing_trip ")
+    List<ThemeVo> getHealingList();
+
+
+}
