67310fafa76286375c8f301c36b2e6f572fdc98f
Eun Ji Shin <159417728+belovelace@users.noreply.github.com>
2024-11-22 22:05:49 +0900

    더미데이터

---
+++ database/더미데이터.sql
@@ -0,0 +1,29 @@
+----더미 데이터--
+----------------
+
+-- AREA 테이블의 더미 데이터 생성
+
+INSERT INTO area (area_id, area_name) VALUES (1, '강원도');
+INSERT INTO area (area_id, area_name) VALUES (2, '광주, 전남');
+INSERT INTO area (area_id, area_name) VALUES (3, '대구 경북');
+INSERT INTO area (area_id, area_name) VALUES (4, '대전, 충남');
+INSERT INTO area (area_id, area_name) VALUES (5, '부산, 경남');
+INSERT INTO area (area_id, area_name) VALUES (6, '인천, 경기');
+INSERT INTO area (area_id, area_name) VALUES (7, '전라북도');
+INSERT INTO area (area_id, area_name) VALUES (8, '제주도');
+INSERT INTO area (area_id, area_name) VALUES (9, '충청북도');
+
+-- CATEGORY 테이블의 더미 데이터 생성
+
+INSERT INTO category (ctg_id, ctg_detail) VALUES (1, '혼자');
+INSERT INTO category (ctg_id, ctg_detail) VALUES (2, '연인');
+INSERT INTO category (ctg_id, ctg_detail) VALUES (3, '가족');
+INSERT INTO category (ctg_id, ctg_detail) VALUES (4, '친구');
+
+-- TRAVEL_THEME 테이블의 더미 데이터 생성
+
+INSERT INTO travel_theme (theme_id, theme) VALUES (1, '미식여행');
+INSERT INTO travel_theme (theme_id, theme) VALUES (2, '힐링여행');
+INSERT INTO travel_theme (theme_id, theme) VALUES (3, '활력여행');
+
+
