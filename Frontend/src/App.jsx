ea0fb09fbdb1fdff1cac5295311099efc3376d14
조하은 <jhe93@naver.com>
2024-11-23 02:31:12 +0900

    add: 위치 context 추가

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,19 +11,31 @@ import KakaoMap from './pages/KakaoMap';
 import HomePage from './pages/HomePage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import MapPage from './pages/MapPage';
+import { LocationProvider } from './context/LocationContext';
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
+            {/* <div style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
+                <Header />
+            </div> */}
             <Header />
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/home" element={<HomePage />} />
                 <Route path="/create-trip" element={<CreateTrip />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/map" element={<KakaoMap />} />
+                <Route
+                    path="/map"
+                    element={
+                        <LocationProvider>
+                            <MapPage />
+                        </LocationProvider>
+                    }
+                />
                 {/* <Route path="/new" element={<New />} /> */}
                 {/* <Route path="/diary/:id" element={<Diary />} /> */}
                 <Route path="*" element={<NotFound />} />
